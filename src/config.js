c56bf29fc2cfcca64e0bb7fc106b83208d1f908b
YvesIraguha <iraguhaivos@gmail.com>
2021-08-25 14:24:00 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+const config = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-attachments-123",
+  },
+  apiGateWay: {
+    REGION: "us-east-1",
+    URL: "https://sislvgxub1.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_WpRnKTcy0",
+    APP_CLIENT_ID: "1vj3f9it54pijufdo0i9aodth6",
+    IDENTITY_POOL_ID: "us-east-1:645bb9e0-f22f-4717-88b2-b3314b5eecb7",
+  },
+};
